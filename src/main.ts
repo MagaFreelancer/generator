6ea66d160be2d6106ecd12c279acf6263e6eb3e1
Magomed Elmurzaev <ban0le4@gmail.com>
2024-09-04 17:42:45 +0300

    layout

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div class="generator">
+      <div class="controller">
+        <div class="controller__kit">
+          <button class="controller__kit-btn">Набор 1</button>
+          <button class="controller__kit-btn">Набор 2</button>
+        </div>
+        <button class="controller__btn" disabled>Сгенерировать</button>
+      </div>
+      <div class="field">
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
